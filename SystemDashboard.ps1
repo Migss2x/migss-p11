7c4056cb851846ce69e46b1ab2e61626b29b5315
Migssgpt <caleb.kiehne2@gmail.com>
2026-03-16 17:04:40 +1100

    SystemDashboard.ps1

--- SystemDashboard.ps1
+++ SystemDashboard.ps1
@@ -3,7 +3,7 @@ Add-Type -AssemblyName System.Drawing
 
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "System Dashboard"
-$form.Size = New-Object System.Drawing.Size(400,300)
+$form.Size = New-Object System.Drawing.Size(500,500)
 $form.StartPosition = "CenterScreen"
 
 $label = New-Object System.Windows.Forms.Label
@@ -12,17 +12,46 @@ $label.Location = New-Object System.Drawing.Point(20,20)
 $label.Font = New-Object System.Drawing.Font("Consolas",10)
 
 function Get-SystemInfo {
+
+    # CPU Usage
     $cpu = (Get-Counter '\Processor(_Total)\% Processor Time').CounterSamples.CookedValue
+
+    # RAM Usage
     $ram = (Get-CimInstance Win32_OperatingSystem)
     $usedRam = [math]::Round(($ram.TotalVisibleMemorySize - $ram.FreePhysicalMemory)/1MB,2)
+
+    # Network Info
+    $netAdapters = Get-NetAdapter | Where-Object Status -eq 'Up'
+    $networkText = ""
+    foreach ($adapter in $netAdapters) {
+        $name = $adapter.Name
+        $status = $adapter.Status
+        $mac = $adapter.MacAddress
+
+        $ips = (Get-NetIPAddress -InterfaceAlias $name | Where-Object {$_.AddressFamily -eq "IPv4"} | Select-Object -ExpandProperty IPAddress) -join ", "
+
+        # Wi-Fi SSID (if Wi-Fi)
+        if ($adapter.MediaType -eq "802.3") {
+            $ssid = ""
+        } elseif ($adapter.MediaType -like "*Wireless*") {
+            $ssidMatch = netsh wlan show interfaces | Select-String "SSID\s*:\s*(.+)$"
+            $ssid = if ($ssidMatch) { $ssidMatch.Matches[0].Groups[1].Value } else { "" }
+        } else {
+            $ssid = ""
+        }
+
+        $networkText += "Adapter: $name`nStatus: $status`nMAC: $mac`nIP(s): $ips`nWi-Fi: $ssid`n`n"
+    }
 
     $text = @"
 Computer: $env:COMPUTERNAME
 User: $env:USERNAME
 
 CPU Usage: $([math]::Round($cpu,2)) %
-
 RAM Used: $usedRam GB
+
+NETWORK INFO:
+$networkText
 
 OS: $($ram.Caption)
 "@
